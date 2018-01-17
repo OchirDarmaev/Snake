0d2bac52f9eecd7302a8d4199ce90d2b5bdd9b2d
Ochir Darmaev <darmaev@rbsoft.ru>
2018-01-18 02:04:50 +0800

    Добавлен метод преобразования в массив

--- Snake/FigureBase.cs
+++ Snake/FigureBase.cs
@@ -9,5 +9,21 @@ namespace SnakeGame
     {
         public List<Point> points;
         public abstract void Draw();
+        protected int[,] ToArray(int x, int y)
+        {
+            int[,] res = new int[x, y];
+           
+                for (int i = 0; i < x ; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                    {
+                    var point = points.Find(p => p.X == i && p.Y == j);
+                    if (point != null)
+                        res[i, j] = (int)point.Figure;
+                    }
+                }
+            return res;
+
+        }
     }
 }
